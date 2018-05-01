18d464bfd4ba0bf0893d43cf1b63aa336f64d68c
RodrigoRov <00357215@uca.edu.sv>
2018-04-30 20:21:11 -0600

    agregue busqueda al app bar

--- app/src/main/java/com/example/carlos/administraciondecuentas/SearchableActivity.java
+++ app/src/main/java/com/example/carlos/administraciondecuentas/SearchableActivity.java
@@ -2,9 +2,13 @@ package com.example.carlos.administraciondecuentas;
 
 import android.app.ListActivity;
 import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -15,11 +19,11 @@ public class SearchableActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchable);
 
-        /*Intent srcIntent = getIntent();
+        Intent srcIntent = getIntent();
         if(Intent.ACTION_SEARCH.equals(srcIntent.getAction())){
             String query = srcIntent.getStringExtra(SearchManager.QUERY);
 
-        }*/
+        }
         ArrayList<Producto> productos = new ArrayList<>();
         productos.add(new Producto("Kerla", 2.00));
         productos.add(new Producto("Adri", 31.14));
@@ -28,4 +32,21 @@ public class SearchableActivity extends ListActivity {
         ListView items = findViewById(R.id.List_view_items);
         items.setAdapter(adapter);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+        searchView.setSubmitButtonEnabled(true);
+
+        return true;
+    }
+
+
 }
