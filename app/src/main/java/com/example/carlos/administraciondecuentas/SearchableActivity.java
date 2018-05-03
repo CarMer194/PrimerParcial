b260bea049991724936aa7676ea33f549acdd633
RodrigoRov <00357215@uca.edu.sv>
2018-05-02 22:59:59 -0600

    Funciona el search

--- app/src/main/java/com/example/carlos/administraciondecuentas/SearchableActivity.java
+++ app/src/main/java/com/example/carlos/administraciondecuentas/SearchableActivity.java
@@ -3,10 +3,9 @@ package com.example.carlos.administraciondecuentas;
 import android.app.ListActivity;
 import android.app.SearchManager;
 import android.content.Context;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.widget.ListView;
@@ -14,23 +13,18 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class SearchableActivity extends ListActivity {
+    ArrayList<Producto> productos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchable);
 
-        Intent srcIntent = getIntent();
-        if(Intent.ACTION_SEARCH.equals(srcIntent.getAction())){
-            String query = srcIntent.getStringExtra(SearchManager.QUERY);
 
-        }
-        ArrayList<Producto> productos = new ArrayList<>();
+        productos = new ArrayList<>();
         productos.add(new Producto("Kerla", 2.00));
         productos.add(new Producto("Adri", 31.14));
         productos.add(new Producto("Perrow", 13.42));
-        CustomListAdapter adapter = new CustomListAdapter(this, productos);
-        ListView items = findViewById(R.id.List_view_items);
-        items.setAdapter(adapter);
+
     }
 
     @Override
@@ -41,12 +35,33 @@ public class SearchableActivity extends ListActivity {
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
         // Assumes current activity is the searchable activity
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        Log.d("Component Name",getComponentName().toString());
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(this.getComponentName()));
         searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
         searchView.setSubmitButtonEnabled(true);
 
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                doMyQuery(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
         return true;
     }
 
-
+    public void doMyQuery(String query){
+        for (Producto p :productos){
+            if(p.getName().equals(query)){
+                CustomListAdapter adapter = new CustomListAdapter(this, productos);
+                ListView items = findViewById(R.id.List_view_items);
+                items.setAdapter(adapter);
+            }
+        }
+    }
 }
