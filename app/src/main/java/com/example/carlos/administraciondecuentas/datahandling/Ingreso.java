8e3f747eccbfc6e19103e8aeaf4f2a0acde39778
jossehblanco <jossehblanco@gmail.com>
2018-05-04 22:58:20 -0600

    Se creo la clase ingreso para luego ser utilizada en el datahandler

---
+++ app/src/main/java/com/example/carlos/administraciondecuentas/datahandling/Ingreso.java
@@ -0,0 +1,12 @@
+package com.example.carlos.administraciondecuentas.datahandling;
+
+import java.util.HashMap;
+
+/**
+ * Created by UCA on 4/5/2018.
+ */
+
+public class Ingreso {
+    private HashMap<String, String> ingresoProducto;
+    private String nombreCuenta;
+}
